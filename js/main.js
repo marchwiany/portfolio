ab5ae9346636d4d599311380ebc4f755219c3e4d
marchwiany <monique@launchpadlab.com>
2014-09-04 10:26:37 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	// Cache the Window object
+	$window = $(window);
+                
+   $('section[data-type="background"]').each(function(){
+     var $bgobj = $(this); // assigning the object
+                    
+      $(window).scroll(function() {
+                    
+		// Scroll the background at var speed
+		// the yPos is a negative value because we're scrolling it UP!								
+		var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+		
+		// Put together our final background position
+		var coords = '50% '+ yPos + 'px';
+
+		// Move the background
+		$bgobj.css({ backgroundPosition: coords });
+		
+}); // window scroll Ends
+
+ });	
+
+
+
+
+$(".slidingDiv").hide();
+	$(".show_hide").show();
+	
+	$('.show_hide').click(function(){
+	$(".slidingDiv").slideToggle();
+	});
+
+
+
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 71) {
+      $('.nav').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 72) {
+      $('.nav').removeClass('navbar-fixed');
+    }
+  });
+
+
+}); 
+
+
